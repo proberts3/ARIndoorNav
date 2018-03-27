a5ee499efbf1dea17aae7cd22e97c04f526b4372
Edith Li <abenoy05@gmail.com>
2018-03-28 05:38:57 +0800

    add input scene

---
+++ Assets/ClickOnSceneChange.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickOnSceneChange : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnClick()
+	{
+		SceneManager.LoadScene("vuforiaScene", LoadSceneMode.Single);
+        Debug.Log("scene switched");
+	}
+}
